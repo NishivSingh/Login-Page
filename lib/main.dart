aca1be4530ca01f3e98d8aa6494b48f7bf67b021
Nishiv Singh <singh.136@iitj.ac.in>
2023-04-19 23:22:21 +0530

    Created the function required for checking th ecurrent state of user's data

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:sleeksites_login_page/login_page.dart';
+import 'package:sleeksites_login_page/secret_page.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   final storage = const FlutterSecureStorage();
+  @override
+  void initState() {
+    super.initState();
+    checkLoginStatus();
+  }
+
+  void checkLoginStatus() async {
+    String? username = await storage.read(key: 'username');
+    String? password = await storage.read(key: 'password');
+
+    if (username != null && password != null) {
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacementNamed(context, '/secret');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +37,7 @@ class _MyAppState extends State<MyApp> {
       initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(),
+        '/secret': (context) => SecretPage(),
       },
     );
   }
