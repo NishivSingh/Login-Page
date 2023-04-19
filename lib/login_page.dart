a60f6efd84f699cba922f5305beeb0c8861ef0f6
Nishiv Singh <singh.136@iitj.ac.in>
2023-04-19 23:55:54 +0530

    Changed the widgets to create more appealing UI for better user experience

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -9,47 +11,114 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
-  final storage = FlutterSecureStorage();
+  final storage = const FlutterSecureStorage();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Username',
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: Neumorphic(
+                  style: const NeumorphicStyle(
+                    depth: -8,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 32,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const Text(
+                          'Welcome back!',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 32),
+                        Neumorphic(
+                          style: NeumorphicStyle(
+                            depth: -8,
+                            boxShape: NeumorphicBoxShape.roundRect(
+                              BorderRadius.circular(16),
+                            ),
+                          ),
+                          child: TextField(
+                            controller: usernameController,
+                            decoration: const InputDecoration(
+                              hintText: 'Username',
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 12,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Neumorphic(
+                          style: NeumorphicStyle(
+                            depth: -8,
+                            boxShape: NeumorphicBoxShape.roundRect(
+                              BorderRadius.circular(16),
+                            ),
+                          ),
+                          child: TextField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              hintText: 'Password',
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 12,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 32),
+                        NeumorphicButton(
+                          onPressed: () async {
+                            await storage.write(
+                                key: 'username',
+                                value: usernameController.text);
+                            await storage.write(
+                                key: 'password',
+                                value: passwordController.text);
+                            // ignore: use_build_context_synchronously
+                            Navigator.pushReplacementNamed(context, '/secret');
+                          },
+                          style: NeumorphicStyle(
+                            depth: 4,
+                            boxShape: NeumorphicBoxShape.roundRect(
+                              BorderRadius.circular(16),
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 12),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(height: 16),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                ),
-              ),
-              SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: () async {
-                  await storage.write(
-                      key: 'username', value: usernameController.text);
-                  await storage.write(
-                      key: 'password', value: passwordController.text);
-                  Navigator.pushReplacementNamed(context, '/secret');
-                },
-                child: Text('Login'),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/secret_page.dart
+++ lib/secret_page.dart
@@ -2,37 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecretPage extends StatefulWidget {
+  const SecretPage({Key? key}) : super(key: key);
+
   @override
   _SecretPageState createState() => _SecretPageState();
 }
 
 class _SecretPageState extends State<SecretPage> {
-  final storage = FlutterSecureStorage();
+  final storage = const FlutterSecureStorage();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Secret Page'),
+        title: const Text('Secret Page'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.purple,
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Welcome to the Secret Page!'),
-              SizedBox(height: 32),
-              ElevatedButton(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple, Colors.deepPurple],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 70,
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.lock_outline,
+                size: 80,
+                color: Colors.deepPurple,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Welcome to the Secret Page!',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 50),
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
                 onPressed: () async {
                   await storage.delete(key: 'username');
                   await storage.delete(key: 'password');
                   Navigator.pushReplacementNamed(context, '/');
                 },
-                child: Text('Logout'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  elevation: 5,
+                ),
+                child: const Text(
+                  'Logout',
+                  style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
